73b4a80088e6c775082e769386898ba42c1780c8
divyansh-1901 <89969262+divyansh-1901@users.noreply.github.com>
2022-12-28 13:13:53 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from pytest import main
+
+from arithmetic_arranger import arithmetic_arranger
+
+
+print(arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True))
+
+
+# Run unit tests automatically
+main()
